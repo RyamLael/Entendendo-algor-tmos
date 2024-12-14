ea7ba7cda36deabb733f69bdd64538d6858255dc
Ryam Lael <ryyamlael@gmail.com>
2024-12-14 18:53:04 -0300

    :books: Read cap1

---
+++ 01-Introdução a algoritmos/binary_search.py
@@ -0,0 +1,23 @@
+def binary_search(list: list, item) -> int:
+    """
+    Recebe uma lista ordenada e um elemento.
+    Caso o elemento esteja na lista, retorna a posição dele, caso não esteja, retorna None.
+    """
+
+    start = 0
+    end = len(list)
+
+    while (start <= end):
+
+        mid = (start + end)//2
+        attempt = list[mid]
+
+        if attempt == item:
+            return mid
+
+        if mid < item:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return None
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Entendendo-algoritmos
 Repositório destinado ao estudo e exercício do livro: Entendendo algoritmos, escrito por Aditya Y. Bhargavaa.
+
+![alt text](images/capa.png)
